34ea0ee81e7887a8f70f2da17be6b5126320ca5a
Marco Aceti <mail@marcoaceti.it>
2021-05-05 15:49:25 +0200

    Code restyling

--- easystaff/parser.py
+++ easystaff/parser.py
@@ -114,27 +114,29 @@ class SiLab:
 
     def get_slots(self):
         resp = self.session.post(const.ENDPOINT_SILAB_SLOTS)
+        assert resp.status_code == 200
         return resp.json()["rooms"]
 
-    def login(self, user, pwd):
+    def login(self, username, password):
         resp = self.session.post(
                 const.ENDPOINT_SILAB_LOGIN,
                 data={
-                    "username": user,
-                    "password": pwd
+                    "username": username,
+                    "password": password
                 }
         )
+        assert resp.status_code == 200
         self.token = resp.json()["jwt"]
-        if self.token != "":
-            return True
-        self.token = None
-        return False
+        assert self.token != ""
 
-    def book_slot(slotid):
+    def book_slot(self, slotid):
+        assert self.token is not None
+
         resp = self.session.post(
                 const.ENDPOINT_SILAB_BOOK + slotid,
                 headers={
-                    "authorization": "Bearer " + token
+                    "authorization": "Bearer " + self.token
                 }
         )
+        assert resp.status_code == 200
         return True if resp.json()["status"] == "success" else False
